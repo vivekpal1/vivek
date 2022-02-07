fc815f8887a122a60ad83e6d314ccec3cd1d7a6f
Vivek Pal <88021457+vivekpal1@users.noreply.github.com>
2022-02-07 21:38:37 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<meta name="keywords" content="Vivek Pal, vivek, web developer, create websites, JavaScript" />
+				<meta name="author" content="Vivek Pal" />
 				<link rel="icon" type="image/png" href="/favicon.png" />
 			</Head>
 			<body>
