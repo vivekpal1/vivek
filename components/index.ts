1ac36a7a01536a0672be8decd66a9c470f16eee1
Vivek Pal <88021457+vivekpal1@users.noreply.github.com>
2022-02-18 03:45:06 +0530

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -3,7 +3,6 @@ export * as Blog from './Blog';
 export * as Button from './Button';
 export * as List from './List';
 export * as Navbar from './Navbar';
-export * as Pill from './Pill';
 export * as Status from './Status';
 
 export { Event } from './Event.component';
