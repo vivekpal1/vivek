f297bc98653b2bdf31298b8ee59114bfd1bc9b1d
Vivek Pal <88021457+vivekpal1@users.noreply.github.com>
2022-02-18 03:57:05 +0530

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -4,8 +4,5 @@ export * as Button from './Button';
 export * as List from './List';
 export * as Navbar from './Navbar';
 export * as Status from './Status';
-
-export { Event } from './Event.component';
 export { Transition } from './Transition.component';
-
 export * from './Wave.styles';
