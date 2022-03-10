9f9d01812b28b33353b659e9e9575aa7ddc5593d
Vivek Pal <31vivekpal@gmail.com>
2022-03-10 22:02:12 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
 				<meta name="keywords" content="Vivek Pal, vivek, web developer, create websites, JavaScript" />
 				<meta name="author" content="Vivek Pal" />
 				<link rel="icon" type="image/png" href="/favicon.png" />
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7873717544167301"
+     crossorigin="anonymous"></script>
 			</Head>
 			<body>
 				<Main />
