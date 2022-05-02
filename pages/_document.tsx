552f6cd064e9614931336ea738ae5efe8f63da20
Vivek Pal <vivekpal@duck.com>
2022-05-02 21:22:39 +0530

    google script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
 				<meta name="keywords" content="Vivek Pal, vivek, vivek pal, vivek web, vivek linux, web developer, shastraos, Shastra OS, ShastraOS create websites, JavaScript" />
 				<meta name="author" content="Vivek Pal" />
 				<link rel="icon" type="image/png" href="/favicon.png" />
+				<Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7873717544167301"
+     crossorigin="anonymous" />
 			</Head>
 			<body>
 				<Main />
