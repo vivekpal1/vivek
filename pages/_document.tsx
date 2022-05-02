cd3c79a9eba3b29146f3fd272635384d3df1e483
Vivek Pal <vivekpal@duck.com>
2022-05-02 21:29:32 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,13 @@ export default function Document() {
 				<meta name="keywords" content="Vivek Pal, vivek, vivek pal, vivek web, vivek linux, web developer, shastraos, Shastra OS, ShastraOS create websites, JavaScript" />
 				<meta name="author" content="Vivek Pal" />
 				<link rel="icon" type="image/png" href="/favicon.png" />
-				<Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7873717544167301"
-     crossorigin="anonymous" />
+				<Script
+   id="Adsense-id"
+   data-ad-client=ca-pub-7873717544167301"
+   async="true"
+   strategy="beforeInteractive"
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+/>
 			</Head>
 			<body>
 				<Main />
